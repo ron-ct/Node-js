f46a0e6e748146554667b29ac1a1536767d619f3
ron-ct <ronkaranja@outlook.com>
2024-12-28 17:23:07 +0300

    how to serve static assets

--- express/index.js
+++ express/index.js
@@ -13,5 +13,11 @@ app.get('/', (req, res) => {res.send("<h1>Hello I'm Rony</h1>");});
 //another route
 app.get('/about-us', (req, res) => {res.send(`About us page`)})
 
+//serve static assets
+app.use(express.static('static'));
+//log every request to the terminal
+app.use((req, res, next) =>{console.log(req.url);
+    next();
+})
 //start server
 app.listen(configs.port, ()=> {console.log(`Server is running at port ${configs.port}`);});
